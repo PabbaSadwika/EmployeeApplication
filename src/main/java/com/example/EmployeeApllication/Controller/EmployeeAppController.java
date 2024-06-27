36661076b2279248dfc90e2ab016172a6c02f0f2
Pabba Sadwika <pabba.sadwika@globallogic.com>
2024-06-27 11:47:46 +0530

    second commit

--- src/main/java/com/example/EmployeeApllication/Controller/EmployeeAppController.java
+++ src/main/java/com/example/EmployeeApllication/Controller/EmployeeAppController.java
@@ -14,6 +14,8 @@ public class EmployeeAppController {
     @GetMapping
     public String EmployeeApp()
     {
+        System.out.println("Employee details");
+        System.out.println("Employee details");
        return "employee details";
 
     }
